311e11abd44285e0c4c8311182e1cc95a748a841
naxolorca <37961870+naxolorca@users.noreply.github.com>
2018-04-24 16:53:36 -0300

    lab

---
+++ Lab 7/Lab 7/Arbol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    public class Arbol<T>
+    {
+        Nodo<T> fin;
+        public void RetornarArbol()
+        {
+            fin = null;
+        }
+        public void Agregar(int numero)
+        {
+            Nodo<T> nuevo = new Nodo<T>();
+            nuevo.izquierda = null;
+            nuevo.derecha = null;
+            nuevo.numero = numero;
+            if(fin == null) { fin = nuevo; }
+            else
+            {
+
+
+
+            }
+        
+
+
+        }
+    }
+}
